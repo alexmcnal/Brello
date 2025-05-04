9d0883b393a7dd624aadbe79a42d03499445962a
David McNally <david.mcnally@artofsource.com>
2025-05-04 18:07:15 +0100

    Configuring development env

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 echo "Building ..."
 
 bundle check || bundle install
-bundle exec rails db:create db:migrate db:seed development:db:seed db:test:prepare
+bundle exec rails db:create db:migrate db:seed db:test:prepare
