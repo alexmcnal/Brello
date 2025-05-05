00e5c7e5c0ca56ab02dfc77f54bb7eb51091c554
Alex McNally <alexdgmcnally@icloud.com>
2025-05-05 19:33:35 +0100

    Card edit test returns positive result

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,12 +7,32 @@ class CardsController < ApplicationController
     @card = Card.new
   end
 
+  def show
+    @card = Card.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to cards_path
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
   def create
     @card = Card.build(card_params)
     if @card.save
       redirect_to cards_path(@card)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card, notice: "Card updated successfully"
+    else
+      render :edit
     end
   end
 
