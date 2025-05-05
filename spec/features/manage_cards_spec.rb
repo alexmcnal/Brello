2473c03f5da143750ea830fdcc57fb4eb5703bb5
David McNally <david.mcnally@artofsource.com>
2025-05-05 15:43:07 +0100

    Manage cards

--- spec/features/manage_cards_spec.rb
+++ spec/features/manage_cards_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Manage Cards', type: :feature, js: true do
 
-  # describe 'Boar
+  scenario 'index page show all cards' do
+    Card.create!(title: 'Test Card', description: 'This is a test card')
+
+    visit cards_path
+
+    expect(page).to have_content('Test Card')
+  end
 
   describe 'Create a new card' do
     scenario 'new cards appear on the board' do
