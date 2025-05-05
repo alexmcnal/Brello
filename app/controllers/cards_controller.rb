bc4085bb0052bf52ec8cea00ce8c79c1296f3c76
Alex McNally <alexdgmcnally@icloud.com>
2025-05-05 17:02:34 +0100

    Card creation test pass

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,23 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.build(card_params)
+    if @card.save
+      redirect_to cards_path(@card)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :description)
+  end
 end
