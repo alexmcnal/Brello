057381f17a8492733cd93103c4c007f7f04906c7
Alex McNally <alexdgmcnally@icloud.com>
2025-05-07 16:04:42 +0100

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "cards#index"
 
   resources :cards, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
 end
