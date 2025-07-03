0956ff639c61ea04c67cfb3f0a3cd53c3868f9e4
Alex McNally <alexdgmcnally@icloud.com>
2025-07-03 09:54:21 +0100

    basic drag drop functionality

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "draggable", to: "https://cdn.jsdelivr.net/npm/@shopify/draggable@1.0.0-beta.8/lib/draggable.bundle.js"
+
