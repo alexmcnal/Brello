0a9aa7afafa84728c1dfc80326b076bb527928b1
Alex McNally <alexdgmcnally@icloud.com>
2025-07-21 13:15:52 +0100

    model spec for projects

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with a name' do
+      project = Project.new(name: 'My Project', description: 'Test description')
+      expect(project).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      project = Project.new(name: nil, description: 'Test description')
+      expect(project).not_to be_valid
+      expect(project.errors[:name]).to include("can't be blank")
+    end
+  end
 end
