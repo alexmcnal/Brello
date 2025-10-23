cf738131191be183561d5707fe3862b7347d1156
David McNally <david.mcnally@artofsource.com>
2025-10-23 17:27:13 +0100

    refactored drawer controller

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -6,7 +6,31 @@ export default class extends Controller {
   static targets = [ 'content' ]
   static classes = [ 'open']
 
+  open(e) {
+    e.preventDefault()
+    const openDrawerEvent = new CustomEvent("openDrawer", {
+      bubbles: true,
+      detail: {
+        url: this.element.href
+      },
+    })
+
+    this.element.dispatchEvent(openDrawerEvent)
+  }
+
+  close() {
+    const closeDrawerEvent = new CustomEvent("closeDrawer", {
+      bubbles: true,
+      detail: {
+        name: "close drawer",
+      },
+    })
+
+    this.element.dispatchEvent(closeDrawerEvent)
+  }
+
   contentTargetConnected() {
+    console.log("contentTargetConnected")
     this.handleOpenDrawer = this.handleOpenDrawer.bind(this)
     this.handleCloseDrawer = this.handleCloseDrawer.bind(this)
     this.handleEscape = this.handleEscape.bind(this)
@@ -33,7 +57,7 @@ export default class extends Controller {
 
   handleEscape(event) {
     if (event.key === "Escape") {
-      this.closeDrawer()
+      this.close()
     }
   }
 }
