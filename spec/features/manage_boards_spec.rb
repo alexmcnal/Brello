eed37369a68810ddf788d2ee046ba5370ddd108d
David McNally <david.mcnally@artofsource.com>
2025-11-04 12:52:55 +0000

    Fixes manage boards

--- spec/features/manage_boards_spec.rb
+++ spec/features/manage_boards_spec.rb
@@ -22,18 +22,17 @@ RSpec.describe 'Manage Boards', type: :feature, js: true do
 
   scenario 'edited boards values change when edited' do
     visit root_path
-      click_on 'Project 1'
-      click_on 'New Board'
-      fill_in 'Name', with: 'Newly created board'
-      fill_in 'Description', with: 'Newly created board description'
-      click_on('Save Board')
+    click_on 'Project 1'
+    click_on 'New Board'
+    fill_in 'Name', with: 'Newly created board'
+    fill_in 'Description', with: 'Newly created board description'
+    click_on('Save Board')
 
-      click_on('Newly created board')
-      expect(page).to have_content("SHOW PAGE"), "Not on show page"
-      click_on('Edit Board')
-      fill_in 'Name', with: 'Newly created board edited'
-      fill_in 'Description', with: 'Newly Created board description edited'
-      click_on('Save Board')
-      expect(page).to have_content("Newly created board edited"), "edited board hasn't changed when edited"
+    click_on('Newly created board')
+    click_on('Edit Board')
+    fill_in 'Name', with: 'Newly created board edited'
+    fill_in 'Description', with: 'Newly Created board description edited'
+    click_on('Save Board')
+    expect(page).to have_content("Newly created board edited"), "edited board hasn't changed when edited"
   end
 end
